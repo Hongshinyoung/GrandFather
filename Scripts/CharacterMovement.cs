aeafca7c17e275c8a0cd60a46a8de8b2d8248533
ShinYoung hong <gtd4095@naver.com>
2022-08-25 19:11:03 +0900

    m
    애니메이션 오류 수정중

--- Scripts/CharacterMovement.cs
+++ Scripts/CharacterMovement.cs
@@ -4,23 +4,67 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    public float speed = 20;
+    public float speed;
     float hAxis;
     float vAxis;
 
-    Vector3 MoveVector;
+    bool rDown;
+    bool jDown;
+    bool isJump;
+
+
+    Vector3 moveVec;
 
     Animator anim;
+    Rigidbody rigid;
 
 
     void Update()
     {
-        hAxis = Input.GetAxisRaw("Horiziontal");
-        vAxis = Input.GetAxisRaw("Vertical");
+        GetInput();
+        Move();
+        Turn();
+        Jump();
     }
 
     void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody>();
+        anim = GetComponentInChildren<Animator>();
+    }
+
+    void GetInput()
+    {
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        rDown = Input.GetButton("Run");
+        jDown = Input.GetButtonDown("Jump");
+    }
+
+    void Move()
+    {
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
+        transform.position += moveVec * (rDown ? 1.5f : 1f) * Time.deltaTime;
+        anim.SetBool("isWalk", moveVec != Vector3.zero);
+        anim.SetBool("isRun",rDown);
+    }
+
+    void Turn()
+    {
+        //회전 구현 
+        //LookAt : 지정된 벡터를 향해서 회전시켜주는 함수 
+        transform.LookAt(transform.position + moveVec);
+    }
+
+    void Jump()
+    {
+        if (jDown && moveVec == Vector3.zero && !isJump)
+        {
+            rigid.AddForce(Vector3.up * 15, ForceMode.Impulse);
+            anim.SetBool("isJump", true);
+            anim.SetTrigger("doJump");
+            isJump = true;
+
+        }
     }
 }
